b961223a44637ba382f6544c07296a5c0473536c
Mikhail Kravets <michkravets@gmail.com>
2019-02-23 17:22:42 +0200

    Calculate accuracy

--- examples/logistic_regression_raw.py
+++ examples/logistic_regression_raw.py
@@ -15,6 +15,15 @@ def loss(h, y):
     return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
 
 
+def accuracy(d: np.ndarray, y: np.ndarray, threshold=0.5):
+    def _l(e):
+        if e > threshold:
+            return 1
+        return 0
+    _d = np.array(list(map(_l, d))).reshape(y.shape)
+    return (y == _d).mean()
+
+
 if __name__ == '__main__':
     url = "https://raw.githubusercontent.com/animesh-agarwal/Machine-Learning/master/LogisticRegression/data/marks.txt"
     content = requests.get(url).content
@@ -29,7 +38,7 @@ if __name__ == '__main__':
 
     theta = np.random.uniform(-0.5, 0.5, size=(train_x.shape[1], 1))
 
-    EPOCHS = 150_000
+    EPOCHS = 200_000
     learning_rate = 0.01
     m = len(train_x)
     for i in range(EPOCHS):
@@ -41,5 +50,5 @@ if __name__ == '__main__':
         theta -= learning_rate * grad
 
     res_d = sigmoid(train_x.dot(theta))
-    print(loss(res_d, train_y))
     print(theta)
+    print(accuracy(res_d, train_y))
